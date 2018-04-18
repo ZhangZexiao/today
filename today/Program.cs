94e95cd8820fb28e7c32f4cd38703eeb061cd812
Zhang Zexiao <zhangzexiaozhang@163.com>
2018-04-18 16:16:44 +0800

    Add files via upload

---
+++ today/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace today
+{
+	class Program
+	{
+		[System.STAThread]
+		static void Main(string[]args)
+		{
+			System.Windows.Clipboard.SetText(System.DateTime.Now.ToString("yyyyMMdd"));
+		}
+	}
+}
